f53ed3cd38fb81f5081999397cf761dcceacda60
ladorayn <lado.rayhan@ui.ac.id>
2023-04-18 08:20:09 +0700

    Initial Commit

---
+++ UserAdminApp/UserAdminAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAdminAppApp.swift
+//  UserAdminApp
+//
+//  Created by Lado Rayhan on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct UserAdminAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
